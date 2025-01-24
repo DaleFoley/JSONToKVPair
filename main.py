39fea1ae20a3b39d177024a3ffb1ef0f5f7c2b08
DaleFoley <dale.foley@live.com.au>
2025-01-24 17:58:33 +1100

    argument for env, logic to check env in JSON.

--- main.py
+++ main.py
@@ -3,40 +3,49 @@ import sys
 import os
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print("Require one argument defining the key and another defining the value to build a key:value pair file."
               " Require file name. Example: JSONToKVPair name value C:/path/to/file.json")
         exit()
 
-    key_arg = sys.argv[1]
-    value_arg = sys.argv[2]
-    file_name = sys.argv[3]
+    SCOPE_NODE = 'Scope'
+    ENVIRONMENT_NODE = 'Environment'
 
-    fileToProcess = open(file_name)
+    keyArg = sys.argv[1]
+    valueArg = sys.argv[2]
+    fileNameArg = sys.argv[3]
+    envToProcessArg = sys.argv[4]
+
+    fileToProcess = open(fileNameArg)
     jsonFileToProcess = json.load(fileToProcess)
     fileToProcess.close()
 
     fileToWrite = ""
 
     for key in jsonFileToProcess['Variables']:
-        if key_arg in key and value_arg in key:
-            if key[key_arg] is not None:
-                fileToWrite += key[key_arg] + ":"
-            else:
-                fileToWrite += "Null:"
+        # If environment present check it against the users sysarg, else if environment present and empty.
+        # Value to be processed under both conditions.
+        isProcessJSONValue = ((SCOPE_NODE in key and ENVIRONMENT_NODE in key[SCOPE_NODE]
+                              and key[SCOPE_NODE][ENVIRONMENT_NODE][0] == envToProcessArg)
+                              or (SCOPE_NODE in key and len(key[SCOPE_NODE]) == 0))
 
-            if key[value_arg] is not None:
-                fileToWrite += key[value_arg] + "," + "\n"
-            else:
-                fileToWrite += "Null" + "," + "\n"
-        else:
-            raise ValueError("key and/or value argument was not found in JSON file.")
+        if isProcessJSONValue:
+            if keyArg in key and valueArg in key:
+                if key[keyArg] is not None:
+                    fileToWrite += key[keyArg] + ":"
+                else:
+                    fileToWrite += "Null:"
+
+                if key[valueArg] is not None:
+                    fileToWrite += key[valueArg] + "," + "\n"
+                else:
+                    fileToWrite += "Null" + "," + "\n"
 
     if fileToWrite[-2:] == ',\n':
         fileToWrite = fileToWrite[:-2]
         fileToWrite += '\n'
 
-    savedFilePath = os.path.splitext(os.path.basename(file_name))[0] + ".txt"
+    savedFilePath = os.path.splitext(os.path.basename(fileNameArg))[0] + ".txt"
 
     savedFile = open(savedFilePath, 'w')
     savedFile.write(fileToWrite)
